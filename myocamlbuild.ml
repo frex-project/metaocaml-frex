5180539773fcd915da3b8ee11b9e0e89d5006021
Jeremy Yallop <yallop@gmail.com>
2023-08-02 07:24:04 +0100

    Update opam; fix test build; support 4.02.1+modular-implicits-ber

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -5,7 +5,7 @@ dispatch begin
   function
   | After_rules ->
      begin match Sys.ocaml_version with
-     | "4.04.0" ->
+     | "4.02.1+dev0-2014-08-29" | "4.04.0" ->
         flag ["ocaml"; "compile"; "open_print_code"] & S[A"-open"; A"Print_code"]
      | "4.07.1" | "4.11.1" | "4.14.1" ->
         flag ["ocaml"; "compile"; "open_print_code"] & S[A"-open"; A"Codelib"]
